c535b18be6864735363965efbbd3f3c4090d3fc6
MrDrennan <MrDrennan@users.noreply.github.com>
2018-01-11 16:31:55 -0800

    Added GetAllBooks() method to BookDB

--- BookLibrary/BookDB.cs
+++ BookLibrary/BookDB.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BookLibrary
 {
-    class BookDB
+    static class BookDB
     {
+        public static List<Book> GetAllBooks()
+        {
+            SqlCommand selCmd = new SqlCommand
+            {
+                CommandText =
+                "SELECT ISBN, Price, Title "
+                + "FROM Book"
+            };
+
+            using (selCmd.Connection = DBHelper.GetConnection())
+            {
+                selCmd.Connection.Open();
+                SqlDataReader rdr = selCmd.ExecuteReader();
+                List<Book> bookList = new List<Book>();
+
+                while (rdr.Read())
+                {
+                    Book currBook = new Book
+                    {
+                        ISBN = (string)rdr["ISBN"],
+                        Price = (decimal)rdr["Price"],
+                        Title = (string)rdr["Title"]
+                    };
+
+                    bookList.Add(currBook);
+                }
+
+                return bookList;
+            }
+        }
     }
 }
