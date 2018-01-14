1d3e3cc4b0f17856f464c7454299314e0d5ac026
MrDrennan <MrDrennan@users.noreply.github.com>
2018-01-13 21:06:29 -0800

    Added DBHelper class

---
+++ BookLibrary/DBHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary
+{
+    /// <summary>
+    /// Responsible for general interactions with the Library database
+    /// </summary>
+    static class DBHelper
+    {
+        /// <summary>
+        /// returns a connection to the Library database
+        /// </summary>
+        /// <returns>SqlConnection object for the desired database</returns>
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=LibraryDB;Integrated Security=True");
+        }
+    }
+}
